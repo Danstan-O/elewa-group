9f073e48d3991cf1e0db6e699116f3e30d33f0c0
danstan <oduordanstan@gmail.com>
2023-02-19 11:31:11 +0300

    :art: introduced spacing for readability

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 
-
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule, LayoutModule],
+  imports: [CommonModule,SocialImpactRoutingModule,LayoutModule],
   declarations: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
